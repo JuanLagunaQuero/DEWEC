6dc93eeb6c85ae5f728ebfbfadbbbaa43410a508
Juan Laguna Quero <jlagque616@g.educaand.es>
2022-01-24 12:43:08 +0100

    JQuery-1

---
+++ paises/js/ejemplo.js
@@ -0,0 +1,29 @@
+$(function () {
+    var continentes = $("#continentes");
+    var paises = $("#paises");
+    var resultados = $("#resultado");
+
+    function cargarPais(pais) {
+        return function () {
+            resultados.empty();
+            resultados.text(JSON.stringify(pais));
+        }
+    }
+
+    var lista = continentes.find("option").on("click", function () {
+
+        paises.empty();
+
+        $.getJSON("https://restcountries.com/v3.1/region/" + $(this).val(),
+            function (data, textStatus, jqXHR) {
+                $.each(data, function (ind, valor) {
+                    $("<option>")
+                        .text(valor.translations.spa.official)
+                        .click(cargarPais(valor))
+                        .appendTo(paises);
+                })
+            }
+        );
+    })
+    console.log(lista);
+})
